144be9d19df8c420c258632f20d436815067e0fc
Bogdan (Laptop) <qliteproject@gmail.com>
2019-10-09 18:05:26 +0900

    File update. Registration form added.
    Обновлены файлы. Добавлена форма регистрации. Временно вырезана возможность входа/регистрации через ВКонтакте (заморожено на не определённый срок).

--- app/src/main/java/com/example/passkeeper/SignUpForm.java
+++ app/src/main/java/com/example/passkeeper/SignUpForm.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginForm extends AppCompatActivity {
+public class SignUpForm extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_form);
+        setContentView(R.layout.activity_sign_up_form);
     }
 }
